0e1a454428e106ca5f96927ce29f4e2543a670b9
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-06 11:55:15 -0500

    started writing questions to index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const questions = [
-
-];
-
+var inquirer = require("inquirer")
+var fs = require("fs")
 function writeToFile(fileName, data) {
+    
 }
 
 function init() {
+    inquirer.prompt ([
+        {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+        },
+        {
+           type: "input",
+            message: "What is your project description?",
+            name: "description"
+        }
 
+    ])
 }
 
 init();
