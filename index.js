3c9ff3f49c5b67e3e37fef503f16b7c69422e3ee
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-06 13:29:09 -0500

    added more questions to index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
-function writeToFile(fileName, data) {
-    
+function writeToFile(fileName, data) {    
 }
-
 function init() {
     inquirer.prompt ([
-        {
+        { 
         type: "input",
         message: "What is the project title?",
         name: "title"
@@ -16,6 +14,41 @@ function init() {
             message: "What is your project description?",
             name: "description"
         }
+        {
+            type: "input",
+             message: "What is in the Table of Contents?",
+             name: "contents"
+         }
+        {
+            type: "input",
+             message: "Who are the contributors to your project?",
+             name: "contributors"
+         }
+         {
+            type: "input",
+             message: "What tech was used to build this project?",
+             name: "tech"
+         }
+         {
+            type: "input",
+             message: "What is the build status of this project?",
+             name: "buildstatus"
+         }
+         {
+            type: "input",
+             message: "What is the license for this project?",
+             name: "license"
+         }
+         {
+            type: "input",
+             message: "Have any installations been done from this project?",
+             name: "installation"
+         }
+         {
+            type: "input",
+             message: "What email can I send additional questions to?",
+             name: "email"
+         }
 
     ])
 }
