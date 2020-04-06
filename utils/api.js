aad090f83797fcbc079115cc302be80403060af6
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-06 11:37:51 -0500

    set up api call

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,21 @@
+var axios = require("axios")
+
 const api = {
   getUser(username) {
-
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(function(res)  {
+      console.log(res.data);
+    }).catch(function(error) {
+      if (error) {
+        console.log (error)
+      } 
+      else {
+        console.log ("Success!")
+      }
+    })
   }
+  
 };
-
+api.getUser("JillWesterfelhaus")
 module.exports = api;
+
