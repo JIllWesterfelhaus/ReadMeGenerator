3255403f0bddf04b034bcdc911b7227dc9f0e02b
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-06 18:08:43 -0500

    updated license of repo

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 function writeToFile(fileName, data) {[
     title: "Portfolio2";
-    description: "Second version of my portfolio for Northwestern Coding Bootcamp";
+    description: "Second version of my portfolio for Northwestern Coding Bootcamp.";
     contributors: "Jill Westerfelhaus";
-    tech: "HTML, CSS and Bootstrap";
+    tech: "HTML, CSS and Bootstrap.";
     buildstatus: "Build status is complete.";
-    license: "License is to be determined";
+    license: "Licensed under the Unlicense.";
     installation: "No";
     email: "jillwesthaus@gmail.com"]
 }
