2fb5e9e7d0a8ae6819e25de0852364d4b074a9a2
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-07 08:28:42 -0500

    added commas after questions

--- index.js
+++ index.js
@@ -13,40 +13,45 @@ function init() {
             type: "input",
             message: "What is your project description?",
             name: "description"
-        }
+        },
         {
             type: "input",
             message: "Who are the contributors to your project?",
             name: "contributors"
-        }
+        },
          {
             type: "input",
             message: "What tech was used to build this project?",
             name: "tech"
-        }
+        },
          {
             type: "input",
             message: "What is the build status of this project?",
             name: "buildstatus"
-        }
+        },
          {
             type: "input",
             message: "What is the license for this project?",
             name: "license"
-        }
+        },
+        {
+            type: "input",
+            message: "What badges does this project have?",
+            name: "badges"
+        },
          {
             type: "input",
             message: "Have any installations been done from this project?",
             name: "installation"
-        }
+        },
          {
             type: "input",
             message: "What email can I send additional questions to?",
             name: "email"
-        }
+        },
 
     ])
-        .then()
+        .then function(response);
 }
 
 init();
