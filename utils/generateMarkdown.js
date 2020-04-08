3925250c625ede4b912929e068a3068ce9d3fb9e
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-04-08 07:57:59 -0500

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,28 +20,27 @@ generateMarkdown(res,response) {
 <li>Contact for Further Information</li>
 <br>
 <h2>Project Title</h2>
-<p>The title of this project is ________.<p>
+<p>The title of this project is "".<p>
 <h2>Description</h2>
-<p>____user input for description_____</p>
+<p>""</p>
 <h2>Motivation</h2>
-<p>The motivation for this project is __________.</p>
+<p>The motivation for this project is "".</p>
 <h2>Contributors</h2>
-Contributor or contributors for this project are _____.</p>
+Contributor or contributors for this project are "".</p>
 <h2>Tech Used</h2>
-<p>Tech used for this project is _____.</p>
+<p>Tech used for this project is "".</p>
 <h2>Build Status</h2>
-<p>Build status for this project is _______.</p>
+<p>Build status for this project is "".</p>
 <h2>License</h2>
-<p>License for this project is _____.</p>
-<br>
+<p>License for this project is "".</p>
 <h2>Badges</h2>
-Badge or badges for this project are _____.</p>
+Badge or badges for this project are "".</p>
 <h2>Installations</h2>
-Installations done from this project are _____.</p>
+Installations done from this project are "".</p>
 <h2>Credits</h2>
-<p>We wish to thank ______________.</p>
+<p>We wish to thank "".</p>
 <h2>Contact for Additional Questions</h2>
-<p>Please feel free to email ______ if you have additional questions.  Thank you.</p>
+<p>Please feel free to email "" if you have additional questions.  Thank you.</p>
 
 
 
